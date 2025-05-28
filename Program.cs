75844c9b7ae943c64965e6883b2cac5907c42aa5
Sepr0n2 <turpansaid@gmail.com>
2025-05-28 12:47:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n = 0;
+        while (n <= 0)
+        {
+
+            Console.WriteLine("Bu program girdiğin sayıların çift olanlarını yazdıracak");
+            Console.WriteLine("Kaç sayı girecen!");
+            string? inputn = Console.ReadLine();
+            if (int.TryParse(inputn, out n) && n > 0)
+            {
+                System.Console.WriteLine("Sayı başarıyla alındı");
+            }
+            else
+            {
+                System.Console.WriteLine("POZİTİİFFFF GİİİRRRRRR");
+                n = 0;
+            }
+        }
+        int[] sayidizisi = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            System.Console.WriteLine("{0}.sayıyı gir", i + 1);
+            sayidizisi[i] = int.Parse(Console.ReadLine());
+        }
+         System.Console.WriteLine("Çift sayılar");
+        foreach (var sayi in sayidizisi)
+        {
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+                Console.WriteLine("Çıkmak için bir tuşa basın...");
+                Console.ReadKey();
+    }
+}
